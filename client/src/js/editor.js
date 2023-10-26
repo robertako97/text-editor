27ac50ce779e1b490de4c7c2cb2b5ded3ba61e0d
robertako97 <roberto.diazgmx@gmail.com>
2023-10-25 20:54:31 -0700

    completed code

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,12 +1,9 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
-import { getDb, putDb } from './database';
+import { getDb, postDb, deleteDb, putDb } from './database';
 import { header } from './header';
 
 export default class Editor {
   constructor() {
-    const localData = localStorage.getItem('content');
-
-    // check if CodeMirror is loaded
+    // Check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
     }
@@ -22,27 +19,34 @@ export default class Editor {
       tabSize: 2,
     });
 
-    getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
+    // Load data from IndexedDB and set it in the editor
+    getDb()
+      .then((data) => {
+        console.info('Loaded data from IndexedDB, injecting into editor');
 
-      // Ensure data is an array
-      const dataArray = Array.isArray(data) ? data : [];
+        // Ensure data is an array
+        const dataArray = Array.isArray(data) ? data : [];
 
-      // Extract the 'code' property from each object and join them into a single string
-      const content = dataArray.map((item) => item.code).join('\n');
+        // Extract the 'code' property from each object and join them into a single string
+        const content = dataArray.map((item) => item.code).join('\n');
 
-      this.editor.setValue(content || localData || header);
-    });
-
+        this.editor.setValue(content || header);
+      })
+      .catch((error) => {
+        console.error('Error loading data from IndexedDB:', error);
+      });
 
     this.editor.on('change', () => {
-      localStorage.setItem('content', this.editor.getValue());
-    });
+      // Whenever the editor content changes, update or delete the content in IndexedDB
+      const content = this.editor.getValue().trim();
 
-    // Save the content of the editor when the editor itself loses focus
-    this.editor.on('blur', () => {
-      console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content'));
+      if (content === '') {
+        // If the editor content is empty, delete the entry in IndexedDB
+        deleteDb('unique_id'); // Replace 'unique_id' with an appropriate ID
+      } else {
+        // Otherwise, update content in IndexedDB using a unique ID
+        putDb('unique_id', content); // Replace 'unique_id' with an appropriate ID
+      }
     });
   }
 }
